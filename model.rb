bd4230b9baa5cab79f61f01a9d1e1086ed99bd26
Aaron Vera <alu0100537451@ull.edu.es>
2014-10-24 00:00:15 +0100

    prueba 32

--- model.rb
+++ model.rb
@@ -2,7 +2,7 @@ class ShortenedUrl
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, Text
   property :url, Text
-  property :label, Text
+  property :url_corta, Text
+  property :usuario, Text
 end
