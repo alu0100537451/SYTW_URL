9c806190c40830e84c71de131514a9ec61c4a4cf
Aaron Vera <alu0100537451@ull.edu.es>
2014-10-24 08:35:11 +0100

    v7

--- app.rb
+++ app.rb
@@ -18,6 +18,7 @@ DataMapper.setup( :default, ENV['DATABASE_URL'] ||
                             "sqlite3://#{Dir.pwd}/my_shortened_urls.db" )
 DataMapper::Logger.new($stdout, :debug)
 DataMapper::Model.raise_on_save_failure = true 
+DataMapper.setup(:default,ENV['HEROKU_POSTGRESQL_RED_URL'])
 
 require_relative 'model'
 
